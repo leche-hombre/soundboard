b329b9f5f2d0afc4833cc43e88d187d1b65aeddb
leche-hombre <mrjmontgomery@hotmail.com>
2020-03-06 13:16:28 +0000

    Create layout xml for soundboard activity

---
+++ app/src/main/java/com/example/soundboard/SoundBoard.kt
@@ -0,0 +1,12 @@
+package com.example.soundboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SoundBoard : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sound_board)
+    }
+}

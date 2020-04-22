fb15893d2641ea2a5165e7ca3711b3c2da0304c9
leche-hombre <mrjmontgomery@hotmail.com>
2020-04-22 18:03:34 +0100

    Add dialog to support new category creation

--- app/src/main/java/com/example/soundboard/MainActivity.kt
+++ app/src/main/java/com/example/soundboard/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.soundboard
 
+import android.content.DialogInterface
 import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -16,11 +19,28 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     private var recordAudioPermission: Int = 1
+    private var alertDialog: AlertDialog? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
+        alertDialog = this.let {
+            val builder = AlertDialog.Builder(it)
+            val inflater = this.layoutInflater
+
+            builder.setView(inflater.inflate(R.layout.new_category_dialog, null))
+                .setTitle(R.string.title_add_category)
+                .setPositiveButton(R.string.add) { _, _ ->
+
+                }
+                .setNegativeButton(R.string.cancel) { dialog, _ ->
+                    dialog.cancel()
+                }
+
+            builder.create()
+        }
 
         val categoriesAdapter = CategoryListAdapter(this)
         val rvCategories : RecyclerView = findViewById<RecyclerView>(R.id.rv_categories)
@@ -36,6 +56,19 @@ class MainActivity : AppCompatActivity() {
                 val intent = Intent(this, SoundBoard::class.java)
                 startActivity(intent)
             }
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_add -> {
+                this.alertDialog?.show()
+                true
+            }
+            R.id.action_settings -> {
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 

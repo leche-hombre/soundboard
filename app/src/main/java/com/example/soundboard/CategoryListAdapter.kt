3ee0af47df1833a98bee1f0fca50adaddc7c0439
leche-hombre <mrjmontgomery@hotmail.com>
2020-04-24 01:06:08 +0100

    Add edit and remove buttons to category

--- app/src/main/java/com/example/soundboard/CategoryListAdapter.kt
+++ app/src/main/java/com/example/soundboard/CategoryListAdapter.kt
@@ -1,10 +1,13 @@
 package com.example.soundboard
 
 import android.content.Context
+import android.media.Image
+import android.opengl.Visibility
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
@@ -15,7 +18,9 @@ class CategoryListAdapter internal constructor(context: Context, private var cat
     private var categories = emptyList<Category>()
 
     inner class CategoryViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val categoryItemView: TextView = itemView.findViewById(R.id.tv_category)
+        val txCategoryName: TextView = itemView.findViewById(R.id.tv_category)
+        val btnCategoryEdit: ImageButton = itemView.findViewById(R.id.btn_edit)
+        val btnCategoryDelete: ImageButton = itemView.findViewById(R.id.btn_delete)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewHolder {
@@ -25,11 +30,16 @@ class CategoryListAdapter internal constructor(context: Context, private var cat
 
     override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) {
         val current = categories[position]
-        holder.categoryItemView.text = current.categoryName
+        holder.txCategoryName.text = current.categoryName
 
         holder.itemView.setOnLongClickListener {
-            removeCategory(position)
+            holder.btnCategoryEdit.visibility = View.VISIBLE
+            holder.btnCategoryDelete.visibility = View.VISIBLE
             true
+        }
+
+        holder.btnCategoryDelete.setOnClickListener {
+            removeCategory(position)
         }
     }
 

c14b6fc5d240e111c26969a7099d2e9ce2394d7d
leche-hombre <mrjmontgomery@hotmail.com>
2020-05-04 21:42:24 +0100

    Add slide to cancel text and bin image

--- app/src/main/java/com/example/soundboard/DraggableView.kt
+++ app/src/main/java/com/example/soundboard/DraggableView.kt
@@ -4,8 +4,10 @@ import android.app.Activity
 import android.os.Bundle
 import android.view.MotionEvent
 import android.view.View
-import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
 // TODO: Make this extend view, then other view components can extend from this
@@ -15,11 +17,16 @@ class DraggableView : Activity() {
     private var xPositionDelta: Int = 0
     private var xOriginalPosition: Int = 0
 
+    private lateinit var tvSlideToCancel: TextView
+    private lateinit var ivCancel: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.draggable_view)
-        val btnDraggable = this.findViewById<Button>(R.id.btnDrag)
+        val btnDraggable = this.findViewById<FloatingActionButton>(R.id.btnDrag)
         val layoutParams: ConstraintLayout.LayoutParams = btnDraggable.layoutParams as ConstraintLayout.LayoutParams
+        tvSlideToCancel = this.findViewById(R.id.tv_slide_to_cancel)
+        ivCancel = this.findViewById(R.id.iv_cancel)
         xOriginalPosition = layoutParams.leftMargin
         btnDraggable.setOnTouchListener {view, motionEvent -> onTouch(view, motionEvent) }
     }
@@ -32,12 +39,16 @@ class DraggableView : Activity() {
         when(motionEvent.action) {
             MotionEvent.ACTION_DOWN -> {
                 xPositionDelta = xCurrentPosition - layoutParams.leftMargin
+                tvSlideToCancel.visibility = View.VISIBLE
+                ivCancel.visibility = View.VISIBLE
                 setScaleFactor(view, 1.2f)
             }
             MotionEvent.ACTION_UP -> {
                 view.performClick()
                 layoutParams.leftMargin = xOriginalPosition
                 view.layoutParams = layoutParams
+                tvSlideToCancel.visibility = View.INVISIBLE
+                ivCancel.visibility = View.INVISIBLE
                 setScaleFactor(view, 1f)
             }
             MotionEvent.ACTION_MOVE -> {

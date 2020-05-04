33a18ef174bc6e9f59733d74f61c6de1537d156b
leche-hombre <mrjmontgomery@hotmail.com>
2020-05-04 18:05:57 +0100

    Apply a scale factor to the view on drag

--- app/src/main/java/com/example/soundboard/DraggableView.kt
+++ app/src/main/java/com/example/soundboard/DraggableView.kt
@@ -8,6 +8,8 @@ import android.widget.Button
 import androidx.constraintlayout.widget.ConstraintLayout
 
 
+// TODO: Make this extend view, then other view components can extend from this
+// TODO: Create a layout that's like: [bin][timer][draggable button], holding the button scales it up and starts the time
 class DraggableView : Activity() {
 
     private var xPositionDelta: Int = 0
@@ -30,17 +32,25 @@ class DraggableView : Activity() {
         when(motionEvent.action) {
             MotionEvent.ACTION_DOWN -> {
                 xPositionDelta = xPosition - layoutParams.leftMargin
+                setScaleFactor(view, 1.2f)
             }
             MotionEvent.ACTION_UP -> {
                 view.performClick()
                 layoutParams.leftMargin = xOriginalPosition
                 view.layoutParams = layoutParams
+                setScaleFactor(view, 1f)
             }
             MotionEvent.ACTION_MOVE -> {
+                // TODO: Limit to only sliding left
                 layoutParams.leftMargin = xPosition - xPositionDelta
                 view.layoutParams = layoutParams
             }
         }
         return true
+    }
+
+    private fun setScaleFactor(view: View, scaleFactor: Float) {
+        view.scaleX = scaleFactor
+        view.scaleY = scaleFactor
     }
 }

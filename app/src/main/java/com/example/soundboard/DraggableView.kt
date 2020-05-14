8d482a0b279af8475dff2ada02157c8af934d57f
leche-hombre <mrjmontgomery@hotmail.com>
2020-05-14 17:42:53 +0100

    Only show draggable path when dragging

--- app/src/main/java/com/example/soundboard/DraggableView.kt
+++ app/src/main/java/com/example/soundboard/DraggableView.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.os.Bundle
 import android.view.MotionEvent
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
